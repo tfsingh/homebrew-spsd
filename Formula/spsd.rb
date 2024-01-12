1f2aac2d3214bd95b24582153379d7b5a4c0a4d0
Tej Singh <tejfsingh@gmail.com>
2024-01-12 17:07:05 +0000

    initial commit

---
+++ Formula/spsd.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class spsd < Formula
+    desc "state persistent serverless devboxes"
+    homepage "https://github.com/tfsingh/spsd"
+    url "https://github.com/tfsingh/spsd/releases/download/v0.1.2/spsd-mac.tar.gz"
+    sha256 "ddec96a9ab85227fb6f9a9f45fe45fa73de3a41af7c2c68c0da165c2ffb96e9d"
+    version "0.1.2"
+
+    depends_on "flyctl"
+  
+    def install
+      bin.install "spsd"
+    end
+  end
