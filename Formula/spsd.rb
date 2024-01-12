fcaede547bcf6276ecd1af10f3cfa0a3963c22e5
Tej Singh <tejfsingh@gmail.com>
2024-01-12 17:10:43 +0000

    fix class name

--- Formula/spsd.rb
+++ Formula/spsd.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class spsd < Formula
+class Spsd < Formula
     desc "state persistent serverless devboxes"
     homepage "https://github.com/tfsingh/spsd"
     url "https://github.com/tfsingh/spsd/releases/download/v0.1.2/spsd-mac.tar.gz"
@@ -9,7 +9,7 @@ class spsd < Formula
     version "0.1.2"
 
     depends_on "flyctl"
-  
+
     def install
       bin.install "spsd"
     end
